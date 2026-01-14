c2bfed1e2441c1c5c46e91ba9ab45e66703c4492
Mohd Maaz Azhar <maazazhar9@gmail.com>
2026-01-14 23:04:56 +0530

    chore: configure Qdrant client connection

---
+++ src/config/qdrant.js
@@ -0,0 +1,8 @@
+const { QdrantClient } = require('@qdrant/js-client-rest');
+
+const qdrant = new QdrantClient({
+  url: process.env.QDRANT_URL,
+  apiKey: process.env.QDRANT_API_KEY,
+});
+
+module.exports = qdrant;
